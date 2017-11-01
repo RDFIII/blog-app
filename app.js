a0d095eb5d428ac427a0c35888fa30c8386eea7f
RDF <foulkesr@gmail.com>
2017-11-01 14:36:24 -0400

    added semantic ui cdn, header, footer partials, public/stylesheet

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/blog-app");
+mongoose.connect("mongodb://localhost/blog-app", { useMongoClient: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,7 +19,27 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//   title: "Larivee",
+//   image: "https://www.sheltonsguitars.com/images/2010/5-29-10/larrivee%20d03/big/big%20larrivee%20d03DSCN9225.jpg",
+//   body: "Just got my new Larivee D03.  It is pretty dang sweet.  Got it on the cheap too!"
+// });
+
 // RESTFUL ROUTES
+
+app.get("/", function(req, res){
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+  Blog.find({}, function(err, blogs){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("index", {blogs: blogs});
+    };
+  });
+});
 
 app.listen(3000, function(){
   console.log("Listening on 3000");
